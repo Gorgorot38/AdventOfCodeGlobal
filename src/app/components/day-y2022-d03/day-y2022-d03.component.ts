6db9757a22eaf5dd0c886f370b1d0e30fc59b0ed
unknown <gorgorot38@gmail.com>
2022-12-03 14:27:43 +0100

    Day03 completed

--- src/app/components/day-y2022-d03/day-y2022-d03.component.ts
+++ src/app/components/day-y2022-d03/day-y2022-d03.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, OnChanges, OnDestroy, OnInit, Output, SimpleChanges } from '@angular/core';
+import { Component, EventEmitter, Input, OnDestroy, OnInit, Output } from '@angular/core';
 import { Subject } from 'rxjs';
 import { filter, takeUntil } from 'rxjs/operators';
 import { SolverService } from 'src/app/services/solver.service';
@@ -8,10 +8,13 @@ import { SolverService } from 'src/app/services/solver.service';
   templateUrl: './day-y2022-d03.component.html',
   styleUrls: ['./day-y2022-d03.component.scss'],
 })
-export class DayY2022D03Component implements OnInit, OnChanges, OnDestroy {
+export class DayY2022D03Component implements OnInit, OnDestroy {
   @Input() data: string[] = [];
 
   @Output() result: EventEmitter<string> = new EventEmitter<string>();
+
+  backPacks: string[][] = [];
+  groupedBackPacks: string[][] = [];
 
   private readonly _destroying = new Subject<void>();
 
@@ -29,18 +32,68 @@ export class DayY2022D03Component implements OnInit, OnChanges, OnDestroy {
       });
   }
 
-  ngOnChanges(changes: SimpleChanges): void {
-    if (changes['data']) {
-      //do stuff
+  initVariables() {
+    this.backPacks = this.data
+      .filter((d) => d)
+      .map((d) => {
+        const middleIndex = Math.ceil(d.length / 2);
+        return [d.slice(0, middleIndex), d.slice(middleIndex)];
+      });
+
+    this.groupedBackPacks = [];
+    for (let idx = 0; idx < this.data.filter((d) => d).length; idx = idx + 3) {
+      this.groupedBackPacks.push([this.data[idx], this.data[idx + 1], this.data[idx + 2]]);
     }
   }
 
   solvePartOne() {
-    return '';
+    this.initVariables();
+
+    this.result.emit(
+      this.backPacks
+        .reduce((prev, curr) => {
+          const commonChar = this.getCommonCharacter(curr[0], curr[1]);
+          return prev + this.getCharValue(commonChar);
+        }, 0)
+        .toString()
+    );
+  }
+
+  private getCharValue(c: string): number {
+    if (c.toLocaleLowerCase() === c) return c.charCodeAt(0) - 96;
+    return c.charCodeAt(0) - 38;
+  }
+
+  private getCommonCharacterMultiple(s1: string, s2: string, s3: string): string {
+    const firstCommon = this.getCommonCharacters(s1, s2);
+    return this.getCommonCharacter(firstCommon, s3);
+  }
+
+  private getCommonCharacter(s1: string, s2: string): string {
+    for (const c of s1) {
+      if (s2.includes(c)) return c;
+    }
+  }
+
+  private getCommonCharacters(s1: string, s2: string): string {
+    let chars = '';
+    for (const c of s1) {
+      if (s2.includes(c)) chars += c;
+    }
+    return chars;
   }
 
   solvePartTwo() {
-    return '';
+    this.initVariables();
+
+    this.result.emit(
+      this.groupedBackPacks
+        .reduce((prev, curr) => {
+          const commonChar = this.getCommonCharacterMultiple(curr[0], curr[1], curr[2]);
+          return prev + this.getCharValue(commonChar);
+        }, 0)
+        .toString()
+    );
   }
 
   ngOnDestroy(): void {
@@ -48,4 +101,3 @@ export class DayY2022D03Component implements OnInit, OnChanges, OnDestroy {
     this._destroying.complete();
   }
 }
-
