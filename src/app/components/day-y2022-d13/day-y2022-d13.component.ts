4f27140fd55b116e463f9fb179f732aa88ebab80
unknown <gorgorot38@gmail.com>
2022-12-13 10:45:06 +0100

    Day 13 completed - Bit of a struggle on part 1

--- src/app/components/day-y2022-d13/day-y2022-d13.component.ts
+++ src/app/components/day-y2022-d13/day-y2022-d13.component.ts
@@ -1,4 +1,5 @@
 import { Component, EventEmitter, Input, OnChanges, OnDestroy, OnInit, Output, SimpleChanges } from '@angular/core';
+import * as _ from 'lodash';
 import { Subject } from 'rxjs';
 import { filter, takeUntil } from 'rxjs/operators';
 import { SolverService } from 'src/app/services/solver.service';
@@ -8,10 +9,12 @@ import { SolverService } from 'src/app/services/solver.service';
   templateUrl: './day-y2022-d13.component.html',
   styleUrls: ['./day-y2022-d13.component.scss'],
 })
-export class DayY2022D13Component implements OnInit, OnChanges, OnDestroy {
+export class DayY2022D13Component implements OnInit, OnDestroy {
   @Input() data: string[] = [];
 
   @Output() result: EventEmitter<string> = new EventEmitter<string>();
+
+  packets: Packets[] = [];
 
   private readonly _destroying = new Subject<void>();
 
@@ -29,18 +32,47 @@ export class DayY2022D13Component implements OnInit, OnChanges, OnDestroy {
       });
   }
 
-  ngOnChanges(changes: SimpleChanges): void {
-    if (changes['data']) {
-      //do stuff
+  initVariables() {
+    this.packets = [];
+    const filteredData = this.data.filter((d) => d);
+    for (let idx = 0; idx < filteredData.length; idx = idx + 2) {
+      this.packets.push(new Packets(JSON.parse(filteredData[idx]), JSON.parse(filteredData[idx + 1])));
     }
   }
 
   solvePartOne() {
-    return '';
+    this.initVariables();
+
+    let sum = 0;
+    this.packets.forEach((p, idx) => {
+      if (p.compare()) {
+        sum += idx + 1;
+      }
+    });
+
+    this.result.emit(sum.toString());
   }
 
   solvePartTwo() {
-    return '';
+    const modifiedData = this.data.filter((d) => d).map((d) => JSON.parse(d));
+
+    modifiedData.push([[2]]);
+    modifiedData.push([[6]]);
+
+    const sorted = modifiedData.sort((a, b) => {
+      const comp = new Packets(b, a).compare();
+      if (comp) {
+        return 1;
+      } else if (comp === false) {
+        return -1;
+      } else {
+        return 0;
+      }
+    });
+
+    console.log(sorted);
+
+    this.result.emit(((sorted.findIndex((p) => _.isEqual(p, [[2]])) + 1) * (sorted.findIndex((p) => _.isEqual(p, [[6]])) + 1)).toString());
   }
 
   ngOnDestroy(): void {
@@ -49,3 +81,41 @@ export class DayY2022D13Component implements OnInit, OnChanges, OnDestroy {
   }
 }
 
+export class Packets {
+  leftPacket: Array<unknown>;
+  rightPacket: Array<unknown>;
+
+  constructor(leftPacket: Array<unknown>, rightPacket: Array<unknown>) {
+    this.leftPacket = leftPacket;
+    this.rightPacket = rightPacket;
+  }
+
+  compare(): boolean | undefined {
+    let comp: boolean | undefined = undefined;
+    for (let idx = 0; idx < this.getBiggerLength(); idx++) {
+      if (!_.isNil(this.leftPacket[idx]) && _.isNil(this.rightPacket[idx])) {
+        return false;
+      } else if (_.isNil(this.leftPacket[idx]) && !_.isNil(this.rightPacket[idx])) {
+        return true;
+      } else if (Array.isArray(this.leftPacket[idx]) && Array.isArray(this.rightPacket[idx])) {
+        comp = new Packets(this.leftPacket[idx] as Array<unknown>, this.rightPacket[idx] as Array<unknown>).compare();
+      } else if (Array.isArray(this.leftPacket[idx]) && !Array.isArray(this.rightPacket[idx])) {
+        comp = new Packets(this.leftPacket[idx] as Array<unknown>, [this.rightPacket[idx]]).compare();
+      } else if (!Array.isArray(this.leftPacket[idx]) && Array.isArray(this.rightPacket[idx])) {
+        comp = new Packets([this.leftPacket[idx]], this.rightPacket[idx] as Array<unknown>).compare();
+      } else if ((this.leftPacket[idx] as number) < (this.rightPacket[idx] as number)) {
+        return true;
+      } else if ((this.leftPacket[idx] as number) > (this.rightPacket[idx] as number)) {
+        return false;
+      }
+
+      if (comp === false || comp === true) break;
+    }
+    return comp;
+  }
+
+  getBiggerLength() {
+    if (this.leftPacket.length > this.rightPacket.length) return this.leftPacket.length;
+    return this.rightPacket.length;
+  }
+}
